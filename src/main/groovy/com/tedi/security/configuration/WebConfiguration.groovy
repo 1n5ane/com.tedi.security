e664238ef15ea69c257b777d7f8584aa987a1859
1n5ane <th.lolos1000@gmail.com>
2024-08-26 16:41:45 +0300

    Added cors allowed origins to call from frontend

--- src/main/groovy/com/tedi/security/configuration/WebConfiguration.groovy
+++ src/main/groovy/com/tedi/security/configuration/WebConfiguration.groovy
@@ -1,6 +1,7 @@
 package com.tedi.security.configuration
 
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -10,5 +11,15 @@ class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.setUseTrailingSlashMatch(true)
+    }
+
+    //not working
+    @Override
+    void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Replace with the origin(s) you want to allow
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+//                .allowCredentials(true) -> when allowed origins are specific
     }
 }
